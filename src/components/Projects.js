f0f385ac3f215d7964075ae4f5e37f9932ac3441
Segni Bultossa <120930888+psegni@users.noreply.github.com>
2025-12-01 05:45:18 +0300

    Remove duplicate import of motion from framer-motion

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import image from '../project-1.png'; 
-import { motion } from "framer-motion";
 const Projects = () => {
   const projects = [
     {
